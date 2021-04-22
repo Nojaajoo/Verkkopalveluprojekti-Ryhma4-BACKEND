35108890df848d9ab1632e6862a57091825e4ac3
Samppa Kivelä <n0kisa00@students.oamk.fi>
2021-04-22 11:44:55 +0300

    Tilaus

--- order/add.php
+++ order/add.php
@@ -7,7 +7,7 @@ $db = null;
 $input = json_decode(file_get_contents('PHP://input'));
 $snimi = filter_var($input->snimi,FILTER_SANITIZE_STRING);
 $enimi = filter_var($input->enimi,FILTER_SANITIZE_STRING);
-// $osoite = filter_var($input->osoite,FILTER_SANITIZE_STRING);
+$osoite = filter_var($input->osoite,FILTER_SANITIZE_STRING);
 $postinro = filter_var($input->postinro,FILTER_SANITIZE_STRING);
 $postitmp = filter_var($input->postitmp,FILTER_SANITIZE_STRING);
 $puhnro = filter_var($input->puhnro,FILTER_SANITIZE_STRING);
@@ -21,7 +21,7 @@ try {
     ('" . 
         filter_var($enimi,FILTER_SANITIZE_STRING) . "','" . 
         filter_var($snimi,FILTER_SANITIZE_STRING) . "','" . 
-        // filter_var($osoite,FILTER_SANITIZE_STRING) . "','" . 
+        filter_var($osoite,FILTER_SANITIZE_STRING) . "','" . 
         filter_var($postinro,FILTER_SANITIZE_STRING) . "','" . 
         filter_var($postitmp,FILTER_SANITIZE_STRING) . "','" . 
         filter_var($puh,FILTER_SANITIZE_STRING)
